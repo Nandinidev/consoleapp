81fa862b7e9b2be7a2e8918e19337ff979dd4cd7
nandini sushil <Nandinidevi.S@lntinfotech.com>
2020-12-16 09:28:48 +0530

    added console appl

---
+++ SampleGitLearn/SampleGitLearn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleGitLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("helloe world");
+        }
+    }
+}
